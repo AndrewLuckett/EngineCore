d07765972531ffea9da55cd2117718a9672f3e1a
Andrew Luckett <andyluckett9@gmail.com>
2021-05-03 15:18:06 +0100

    Initial Commit

---
+++ Engine/src/global/Version.cpp
@@ -0,0 +1,9 @@
+#include "Version.h"
+
+namespace Engine {
+	extern version CurrentVersion = { 0,0,1 };
+
+	string versionToString(version v) {
+		return std::to_string(v.major) + "." + std::to_string(v.minor) + "." + std::to_string(v.revision);
+	}
+}
